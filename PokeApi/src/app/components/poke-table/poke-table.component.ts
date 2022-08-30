c524f908f694ec456cafcd2ddbb185d8a221e839
Alvaro Murillo H <alvaromurilloh2009@yahoo.com>
2022-08-30 10:30:11 -0600

    Tabla de pokemon

--- PokeApi/src/app/components/poke-table/poke-table.component.ts
+++ PokeApi/src/app/components/poke-table/poke-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-poke-table',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeTableComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['position', 'image', 'name', 'type'];   //Visor de las columnas
+  data: any[] = [];                                                   //Arreglo que recibe los datos de los pokemon
+  dataSource = new MatTableDataSource<any>(this.data);                //Recibidor de los datos
+  pokemons = [];
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor(private pokeService: PokemonService) { }
 
   ngOnInit(): void {
+    this.getPokemons();
   }
 
+  getPokemons(){
+    let pokemonData;
+    for(let i=1; i<=150; i++){
+      this.pokeService.getPokemons(i).subscribe(
+        res => {
+          pokemonData = {
+            position: i,
+            image: res.sprites.front_default,
+            name: res.name
+          };
+          this.data.push(pokemonData);
+          this.dataSource = new MatTableDataSource<any>(this.data);
+          this.dataSource.paginator = this.paginator;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
--- PokeApi/src/app/services/pokemon.service.ts
+++ PokeApi/src/app/services/pokemon.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  baseUrl = environment.baseUrl;        //Url base para los servicios de la PokeApi
+
+  constructor(private http: HttpClient) { }
+
+  getPokemons(index: any){
+    return this.http.get<any>(`${this.baseUrl}/pokemon/${index}`);     //endpoint que devuelve el listado de los pokemon
+  }
 }
